e81b14c5d83fd595b8a9d2f55fe23aef4344af47
İbrahim Ethem Şen <ibrahim-ethem-50@hotmail.com>
2023-02-28 20:10:14 +0300

    solution

--- app/src/main/java/com/ibrahimethemsen/customviewshaderporterduff/ComposeShader.kt
+++ app/src/main/java/com/ibrahimethemsen/customviewshaderporterduff/ComposeShader.kt
@@ -11,21 +11,19 @@ class ComposeShader @JvmOverloads constructor(
 )  : View(context,attributeSet){
     private val moon = BitmapFactory.decodeResource(resources, R.drawable.human)
     private val destinationShader = BitmapShader(moon,Shader.TileMode.CLAMP,Shader.TileMode.CLAMP)
-    private val destinationP = Paint().apply{
-        shader = destinationShader
-    }
+
     private val star = BitmapFactory.decodeResource(resources, R.drawable.heart)
     private val sourceShader = BitmapShader(star,Shader.TileMode.MIRROR,Shader.TileMode.MIRROR)
-    private val sourceP = Paint().apply{
-        shader = sourceShader
-    }
+
     private val composeShaderD = ComposeShader(destinationShader,sourceShader,PorterDuff.Mode.DST)
     private val composeShaderDA = ComposeShader(destinationShader,sourceShader,PorterDuff.Mode.DST_ATOP)
     private val composeShaderDI = ComposeShader(destinationShader,sourceShader,PorterDuff.Mode.DST_IN)
     private val composeShaderDO = ComposeShader(destinationShader,sourceShader,PorterDuff.Mode.DST_OUT)
+    private val composeShaderL = ComposeShader(destinationShader,sourceShader,PorterDuff.Mode.LIGHTEN)
+    private val composeShaderDN = ComposeShader(destinationShader,sourceShader,PorterDuff.Mode.DARKEN)
 
     private val composePaint = Paint().apply {
-        shader = composeShaderDO
+        shader = composeShaderD
     }
 
     override fun onDraw(canvas: Canvas) {
